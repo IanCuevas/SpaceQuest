eb2b3e99f003e420ef741ca86bb8c9a1f2ddd1ec
IanCuevas <iandcuevas28@gmail.com>
2026-03-06 14:37:15 -0700

    Update

---
+++ Assets/Scripts/Animation/AnimationData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimationData", menuName = "Scriptable Objects/AnimationData")]
+public class AnimationData : ScriptableObject
+{
+    public string animationName;
+    public Sprite[] frames;
+    public float frameDelay;
+    public bool loop;
+}
